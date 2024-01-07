814447389af8b9e967d52433c57b93ed135db7e4
Milyaz Kamil <milyazkamil@gmail.com>
2024-01-07 15:59:02 +0300

    Created reusable icon components for easy integration into different parts of the application.

---
+++ src/components/Svg/MoonIcon.jsx
@@ -0,0 +1,16 @@
+const MoonIcon = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 20 20"
+      height="1.5em"
+      width="1.5em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+    </svg>
+  )
+}
+export default MoonIcon;
