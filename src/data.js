32b047e0b9c5669002236d71fe9df4f096ebdbce
Milyaz Kamil <milyazkamil@gmail.com>
2024-01-07 15:40:42 +0300

    Introduce configuration files for voices, usage instructions, and commands. Voices array includes predefined voice options, while 'howToUse' and 'commands' arrays provide guidance on interacting with Jarvis, as well as a list of available commands for users to explore.

---
+++ src/data.js
@@ -0,0 +1,99 @@
+export const voices = [
+  {
+    id: 1,
+    name: 'Milyaz Kamil',
+  },
+  {
+    id: 2,
+    name: 'Aurora',
+  },
+  {
+    id: 3,
+    name: 'Chappie',
+  },
+  {
+    id: 4,
+    name: 'Calista',
+  },
+  {
+    id: 5,
+    name: 'Elly',
+  },
+  {
+    id: 6,
+    name: 'Atlas',
+  },
+  {
+    id: 7,
+    name: 'Nyx',
+  },
+  {
+    id: 8,
+    name: 'Elysia',
+  },
+  {
+    id: 9,
+    name: 'Azula',
+  },
+];
+
+export const howToUse = [
+  {
+    id: 1,
+    title: 'Activate Jarvis:',
+    content:
+      "Click on the center of the Arc Reactor to activate Jarvis. Look for a glow to know it's ready.",
+  },
+  {
+    id: 2,
+    title: 'Speak Your Command:',
+    content:
+      'Once activated, speak your question or command. Jarvis is ready to listen.',
+  },
+  {
+    id: 3,
+    title: 'Listen to Jarvis:',
+    content:
+      "Relax and wait for Jarvis to process your request. You'll hear the AI's response shortly.",
+  },
+  {
+    id: 4,
+    title: 'Explore More:',
+    content:
+      'Want to know more? Feel free to ask anything or explore commands in the app.',
+  },
+];
+
+export const commands = [
+  {
+    id: 1,
+    title: 'Dark Mode:',
+    content: 'Switches to a darker theme for improved visibility in low light.',
+  },
+  {
+    id: 2,
+    title: 'White Mode:',
+    content:
+      'Reverts to a light theme for better visibility in well-lit environments.',
+  },
+  {
+    id: 3,
+    title: 'Switch Mode:',
+    content: 'Quickly toggles between dark and white themes.',
+  },
+  {
+    id: 4,
+    title: 'Change Voice:',
+    content: 'Modifies the voice style of the Jarvis virtual assistant.',
+  },
+  {
+    id: 5,
+    title: 'Open LinkedIn:',
+    content: 'Launches the LinkedIn app.',
+  },
+  {
+    id: 6,
+    title: 'Open Instagram:',
+    content: 'Opens the Instagram app for social media access.',
+  },
+];
